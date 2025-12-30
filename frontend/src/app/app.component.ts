84a441ab5c36ad6891dd0561c4b24fe6790a71c0
Natanesh <nataneshs@gmail.com>
2025-12-30 22:54:57 +0530

    Landing Page Design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth/auth.service';
+import { HeaderRowOutlet } from '../../node_modules/@angular/cdk/table/index';
 @Component({
   selector: 'app-root',
   standalone: true,
